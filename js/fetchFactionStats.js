e5eeaefbdd6e07e8cdc9c244e1feb14c048268a1
mehbark <terezi@pyrope.net>
2024-01-08 23:41:41 -0500

    change faction stats url

--- js/fetchFactionStats.js
+++ js/fetchFactionStats.js
@@ -1,4 +1,4 @@
-GOOGLE_SHEET_FETCH_FACTION_STATS = "https://script.google.com/macros/s/AKfycbzhm1Lvtf5AEiXIswkHmLEs-WSME4rziv8ksLWOrfkaDQ5NVTlfqu9ckgU0NxnE9NsIMQ/exec";
+GOOGLE_SHEET_FETCH_FACTION_STATS = "https://mehbark-rdl-server.deno.dev/faction-stats";
 
 GOOGLE_SHEET_FETCH_PLAYER_STATS = "https://script.google.com/macros/s/AKfycbyMS-qgpSnBpBVMKM8v8_UKf_qIrrvZ2CJRGXvKJQ1xq49SdbHf412_6Kzs8HIsNCMf/exec";
 let factionStats = {};
